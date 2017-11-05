4872765d93cf6e7e79b0e0f2c73cd97b7ed21f9b
Triah <nicolaihedegaardjensen93@gmail.com>
2017-11-05 21:10:30 +0100

    zoom and location stuff

--- app/src/main/java/case1/groupg/raceapp/MainActivity.java
+++ app/src/main/java/case1/groupg/raceapp/MainActivity.java
@@ -6,8 +6,10 @@ import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 public class MainActivity extends Activity {
@@ -20,6 +22,14 @@ public class MainActivity extends Activity {
 
         MapView map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
+
+        map.setBuiltInZoomControls(true);
+        map.setMultiTouchControls(true);
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(9);
+        GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
+        mapController.setCenter(startPoint);
     }
 
     public void onResume(){
