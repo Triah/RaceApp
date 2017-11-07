0f4f753990b359754d6977c80242248a77b38a26
Triah <nicolaihedegaardjensen93@gmail.com>
2017-11-07 19:24:29 +0100

    Semi primitive gps using a bound service architecture

--- app/src/main/java/case1/groupg/raceapp/MainActivity.java
+++ app/src/main/java/case1/groupg/raceapp/MainActivity.java
@@ -1,10 +1,23 @@
 package case1.groupg.raceapp;
 
+import android.Manifest;
 import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.provider.Settings;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import org.osmdroid.api.IMapController;
 import org.osmdroid.config.Configuration;
@@ -13,25 +26,39 @@ import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 public class MainActivity extends Activity {
-    @Override public void onCreate(Bundle savedInstanceState) {
+
+    private static int LOCATION_PERMISSION = 2;
+    public double latitude;
+    public double longitude;
+    GpsLocationListener mService;
+    boolean mBound = false;
+    MapView map;
+    IMapController mapController;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        doPermissions();
         super.onCreate(savedInstanceState);
+
         Context ctx = getApplicationContext();
         //important! set your user agent to prevent getting banned from the osm servers
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
         setContentView(R.layout.activity_main);
 
-        MapView map = (MapView) findViewById(R.id.map);
+        map = (MapView) findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
         map.setBuiltInZoomControls(true);
         map.setMultiTouchControls(true);
 
-        IMapController mapController = map.getController();
+        mapController = map.getController();
         mapController.setZoom(9);
         GeoPoint startPoint = new GeoPoint(48.8583, 2.2944);
         mapController.setCenter(startPoint);
     }
 
+    @Override
     public void onResume(){
         super.onResume();
         //this will refresh the osmdroid configuration on resuming.
@@ -40,4 +67,82 @@ public class MainActivity extends Activity {
         //Configuration.getInstance().save(this, prefs);
         Configuration.getInstance().load(this, PreferenceManager.getDefaultSharedPreferences(this));
     }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Intent intent = new Intent(this, GpsLocationListener.class);
+        bindService(intent, mConnection, Context.BIND_AUTO_CREATE);
+    }
+
+    private void doPermissions() {
+        // If we haven't been granted the permission to use "fine location"
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            } else {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION);
+            }
+        }
+    }
+
+    private ServiceConnection mConnection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service){
+            GpsLocationListener.GetLocationBinder locationService = (GpsLocationListener.GetLocationBinder) service;
+            mService = locationService.getService();
+            mBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0){
+            mBound = false;
+        }
+    };
+
+    //not sure we need this if we want to make it record gps stuff while app is minimized
+    @Override
+    protected void onStop(){
+        super.onStop();
+        if(mBound){
+            unbindService(mConnection);
+            mBound = false;
+        }
+    }
+
+    public void locationSync(View v){
+        if(mBound){
+            latitude = mService.getLatitude();
+            longitude = mService.getLongitude();
+            mapController.setCenter(new GeoPoint(latitude, longitude));
+            mapController.setZoom(17);
+
+        }
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
 }
